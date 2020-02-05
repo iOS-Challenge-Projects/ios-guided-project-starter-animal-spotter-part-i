e88270581c80db2ae3fde36fb2dd50e558bd48ed
fritzgt <tuanis@live.com>
2020-02-05 11:58:36 +0200

    initial commit

--- AnimalSpotter/Application/APIController.swift
+++ AnimalSpotter/Application/APIController.swift
@@ -19,6 +19,17 @@ class APIController {
     private let baseUrl = URL(string: "https://lambdaanimalspotter.vapor.cloud/api")!
     
     // create function for sign up
+    //the return could be empty () or Void which is the same
+    func signUp(with user: User, completion: @escaping (Error?)->()){
+        
+        //Adding "users/signup" to the end of the base URL
+        let signUpURL = baseUrl.appendingPathComponent("users/signup")
+        
+        var request = URLRequest(url: signUpURL)
+        
+        //access the type of HTTP request from the enum
+        request.httpMethod = HTTPMethod.post.rawValue
+    }
     
     // create function for sign in
     
