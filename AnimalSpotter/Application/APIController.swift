f7f0cae79d7196e903576c3c3e77259c51fcaa3f
fritzgt <tuanis@live.com>
2020-02-05 12:46:16 +0200

    completed signup request

--- AnimalSpotter/Application/APIController.swift
+++ AnimalSpotter/Application/APIController.swift
@@ -29,6 +29,43 @@ class APIController {
         
         //access the type of HTTP request from the enum
         request.httpMethod = HTTPMethod.post.rawValue
+        
+        //Specifying the format and the name of the header
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        
+        do{
+            //encode data into a user object
+            let jsonData = try jsonEncoder.encode(user)
+            //Assing the data to the body of the request
+            request.httpBody = jsonData
+            
+        }catch{
+            print("Error encoding data\(error)")
+            completion(error)
+            return
+        }
+        
+        //We get the data, a response ok 201, and an error, all are optional
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            //here we unwrap it and downcasted
+            if let response = response as? HTTPURLResponse, response.statusCode != 200{
+                //send the NSError code in the completion with the statusCode
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error{
+                completion(error)
+                return
+            }
+            
+            //if we pass nil means that there is no errors and we can proceed
+            completion(nil)
+            
+        }.resume()
+        
     }
     
     // create function for sign in
